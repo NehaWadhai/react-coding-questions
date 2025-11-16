985b7a12d699fd8972f80bac0257de52f2d4161e
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-11-17 04:46:12 +0530

    async await and event capturing

--- react-interview-ques/src/jsquestions/debounching.js
+++ react-interview-ques/src/jsquestions/debounching.js
@@ -20,3 +20,43 @@ const myThrottle = (func, limit) => {
     }  
 
 }
+
+
+//===============================  =           ========================
+
+import React, { useState, useEffect, useMemo } from "react";
+
+export default function App() {
+  const [value, setValue] = useState("");
+  const url = "https://fakestoreapi.com/products";
+  const fetchData = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setData(data);
+      });
+  };
+  const [data, setData] = useState([]);
+  const myDebounce = (fn, delay) => {
+    let time;
+    return (...args) => {
+      clearTimeout(time);
+      time = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+  const debounce = useMemo(() => myDebounce(fetchData, 3000), []);
+  const onChange = (e) => {
+    setValue(e.target.value);
+    debounce();
+    // fetch(url).then((res) => console.log('res'))
+  };
+
+  return (
+    <div>
+      <input onChange={onChange}></input>
+    </div>
+  );
+}
