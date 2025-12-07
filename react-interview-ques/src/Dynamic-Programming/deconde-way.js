ca14cbad67358fd0215476aa7755f2b2d1c74a94
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-07 20:53:28 +0530

    Added decode-way problem

---
+++ react-interview-ques/src/Dynamic-Programming/deconde-way.js
@@ -0,0 +1,5 @@
+//Need tp decode string where A=1, B=2,... Z=26
+//To decode a message, digits must be grouped and then mapped back into letters using the reverse of the mapping above. There may be multiple ways to decode a message. For example, "1012" can be mapped into:
+
+//"JAB" with the grouping (10 1 2)
+//"JL" with the grouping (10 12)
