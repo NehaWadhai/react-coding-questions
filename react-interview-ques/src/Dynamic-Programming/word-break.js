2f16450992fe87bc9d8f57b2a486e1af998b2374
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-09 05:21:00 +0530

    Word break problem solution

--- react-interview-ques/src/Dynamic-Programming/word-break.js
+++ react-interview-ques/src/Dynamic-Programming/word-break.js
@@ -10,3 +10,35 @@
 //Input: s = "neetcode", wordDict = ["neet","code"]
 
 //Output: true
+//Need revisit for better understanding
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+         let n = s.length
+        let arr = Array(n+1).fill(false)
+        let map = new Set(wordDict)
+        console.log('map',arr)
+        
+        arr[0] = true
+        let maxLength = 0
+        for(let i =0 ; i < wordDict.length; i++){
+            maxLength = Math.max(maxLength,wordDict[i].length)
+        }
+
+        for(let i =1 ; i <= n ;i++){
+            for(let j= i -1 ; j >= Math.max(0, i - maxLength) ; j--){
+                if(map.has(s.substring(j,i)) && arr[j]){
+                arr[i] = true
+                break
+            }
+            }
+        }
+        console.log('arr',arr)
+        return arr[n]
+    }
+}
