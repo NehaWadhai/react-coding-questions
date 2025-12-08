b89b6c332efc540cf2af6991f0a379f29543a01c
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-08 06:06:27 +0530

    Coin change problem solution

--- react-interview-ques/src/Dynamic-Programming/coin-change.js
+++ react-interview-ques/src/Dynamic-Programming/coin-change.js
@@ -10,3 +10,24 @@
 //Input: coins = [1,5,10], amount = 12
 
 //Output: 3
+
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+        let arr = Array(amount+1).fill(Infinity)
+        arr[0] = 0
+
+        for(let currAmount = 1; currAmount <= amount; currAmount++){
+            for(let coin of coins){
+                if(currAmount - coin >= 0){
+                    arr[currAmount] = Math.min(arr[currAmount], 1 + arr[currAmount - coin])
+                }
+            }
+        }
+        return arr[amount] === Infinity ? -1 : arr[amount];
+    }
+}
