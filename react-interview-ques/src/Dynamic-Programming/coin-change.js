16126b1b33f13d2f87fd2866efe9eec974cdab16
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-08 05:51:25 +0530

    Coin change problem statement

---
+++ react-interview-ques/src/Dynamic-Programming/coin-change.js
@@ -0,0 +1,12 @@
+//you are given integer array coins representing coins of different 
+//denominations and an integer amount representing a total amount of money.
+//Return the fewest number of coins that you need to make up the exact target amount. 
+//If it is impossible to make up the amount, return -1.
+
+//You may assume that you have an unlimited number of each coin.
+
+//Example 1:
+
+//Input: coins = [1,5,10], amount = 12
+
+//Output: 3
