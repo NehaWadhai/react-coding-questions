ea3ea8cc2183c1b2b751dade1ea4746f763b792d
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-08 13:25:36 +0530

    Max product subarray  problem solution

--- react-interview-ques/src/Dynamic-Programming/max-product-subarray.js
+++ react-interview-ques/src/Dynamic-Programming/max-product-subarray.js
@@ -9,3 +9,27 @@
 //Input: nums = [1,2,-3,4]
 
 //Output: 4
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums) {
+        let l = 1
+        let r = 1
+        let ans = nums[0]
+        let n = nums.length
+
+        for(let i = 0; i < nums.length ; i++){
+           
+            l *= nums[i]
+            r *= nums[n - 1 - i]
+            
+            ans = Math.max(ans, Math.max(l,r))
+             l = l == 0 ? 1 : l
+            r = r == 0 ? 1 : r
+        }
+        return ans === -0 ? 0 : ans
+    }
+}
