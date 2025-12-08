2382f69aae83b4d9232e7733951db2d361e85de8
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-09 04:20:13 +0530

    Word break problem statement

---
+++ react-interview-ques/src/Dynamic-Programming/word-break.js
@@ -0,0 +1,12 @@
+// there is string s and dictionary of words dict
+//Given a string s and a dictionary of strings wordDict, 
+//return true if s can be segmented into a space-separated sequence of dictionary words.
+
+//You are allowed to reuse words in the dictionary an unlimited number of times. 
+//You may assume all dictionary words are unique.
+
+//Example 1:
+
+//Input: s = "neetcode", wordDict = ["neet","code"]
+
+//Output: true
