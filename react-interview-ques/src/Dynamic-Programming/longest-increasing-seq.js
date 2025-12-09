69e5cbc762e8ad66036a842aae3ae71302c6ff94
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-10 05:11:24 +0530

    longest increasing sequence problem solution

--- react-interview-ques/src/Dynamic-Programming/longest-increasing-seq.js
+++ react-interview-ques/src/Dynamic-Programming/longest-increasing-seq.js
@@ -9,3 +9,30 @@
 // Output: 4
 // Explanation: The longest increasing subsequence is [1,2,3,7], which has a length of 4.
 
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        let n = nums.length
+        let arr = Array(n).fill(1)
+
+        for(let i =1; i < n ; i++){
+            for(let j = 0 ; j < i; j++){
+                if(nums[j] < nums[i]){
+                    if(arr[j] + 1 > arr[i]){
+                        arr[i] += 1
+                    }
+                    
+                }
+            }
+        }
+        let max = 0
+        for(let i=0 ; i < n ;i++){
+            max = Math.max(arr[i], max)
+        }
+        return max
+    }
+}
