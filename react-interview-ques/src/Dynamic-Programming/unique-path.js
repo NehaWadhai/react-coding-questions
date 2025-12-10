842e1742b41e46fcc06defd47f2ba9aa96aabbc5
Neha Wadhai <nehawadhai@Nehas-MacBook-Air.local>
2025-12-10 05:39:23 +0530

    unique path  problem statement

---
+++ react-interview-ques/src/Dynamic-Programming/unique-path.js
@@ -0,0 +1,6 @@
+// There is an m x n grid where you are allowed to move either down or to the right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that can be taken from the top-left
+// corner of the grid (grid[0][0]) to the bottom-right corner (grid[m - 1][n - 1]).
+
+// You may assume the output will fit in a 32-bit integer.
